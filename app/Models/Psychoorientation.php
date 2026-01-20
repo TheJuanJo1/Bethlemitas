c3daf33d095575371f2f352e50e9d9b3265856d5
The_JuanJo1 <mbappejuan464@gmail.com>
2026-01-20 06:58:38 -0500

    Se arreglo de que no dejaba como cuando uno seleccionaba a que grupo perteneceria el estudiante remitido

--- app/Models/Psychoorientation.php
+++ app/Models/Psychoorientation.php
@@ -9,23 +9,35 @@ class Psychoorientation extends Model
 {
     use HasFactory;
 
+    /**
+     * Campos permitidos para asignación masiva
+     */
     protected $fillable = [
         'psychologist_writes',
         'id_user_student',
+        'age_student',
+        'group_student',
+        'director_group_student',
         'title_report',
         'reason_inquiry',
         'recomendations',
         'date',
     ];
 
-    //Rerlacion con la tabla de docentes para extraer datos de la psicoorientadora
-    public function user_psychology() {
+    /**
+     * Relación con la tabla de docentes
+     * Psicoorientadora que escribe el informe
+     */
+    public function user_psychology()
+    {
         return $this->belongsTo(Users_teacher::class, 'psychologist_writes');
     }
 
-    //Relacion con la tabla estudiantes
-    public function user_student() {
+    /**
+     * Relación con la tabla de estudiantes
+     */
+    public function user_student()
+    {
         return $this->belongsTo(Users_student::class, 'id_user_student');
     }
-    
 }
