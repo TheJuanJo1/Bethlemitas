181383dc50251dad0e25f272e669c6eefc2af3bf
The_JuanJo1 <mbappejuan464@gmail.com>
2026-02-23 14:28:40 -0500

    Se borraon tablas que no se utilizan+

--- database/migrations/2026_02_23_155444_drop_ethnic_groups_table.php
+++ database/migrations/2026_02_23_155444_drop_ethnic_groups_table.php
@@ -11,14 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carer', function (Blueprint $table) {
-            $table->id();
-            $table->string('relationship');
-            $table->string('education_level');
-            $table->string('phone');
-            $table->string('email', '50');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -26,6 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carer');
+        //
     }
 };
