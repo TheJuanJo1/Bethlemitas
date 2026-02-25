8346fcf0ff6a6e5a9f1b6b78de5bc95571446542
The_JuanJo1 <mbappejuan464@gmail.com>
2026-02-25 10:48:37 -0500

    Se agrego la parte de caudno se bloquea a un usuario no lo deje entrar y que cuando se desbloquea ya lo deja

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,13 +3,33 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return redirect()->route('login');  // Redirige a la página de login
+            return route('login');
         }
+    }
+
+    public function handle($request, \Closure $next, ...$guards)
+    {
+        $this->authenticate($request, $guards);
+
+        $user = Auth::user();
+
+        // 🔥 Verificar si está suspendido
+        if ($user && $user->id_state == 2) {
+            Auth::logout();
+
+            return redirect()->route('login')
+                ->withErrors([
+                    'blocked' => 'Tu cuenta se encuentra suspendida. Comunícate con el Director.'
+                ]);
+        }
+
+        return $next($request);
     }
 }
